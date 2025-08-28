642576db8e8921f94673b35fe2c35f578f3cee57
Saheeb Ahanger <saheebahanger1@gmail.com>
2025-08-28 18:13:29 +0530

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,64 @@
+# Printing to the console
+
+print("Hello Saheeb!")
+#output= Hello Saheeb!
+
+
+# Printing sentences in the next line 
+
+print("Hello Saheeb! \n Hello World ")
+#output= Hello Saheeeb!
+#Hello World
+
+
+# Concatenaion of two or more strings
+
+print("Hello " + "Saheeb")
+#output= Hello Saheeb
+
+
+#Taking input from user
+
+input("What is your Name?")
+#output= what is your Name? _ Saheeb
+
+
+#Making variables
+
+Name = "Saheeb"
+print(Name)
+#output= Saheeb
+
+
+#Calculating Length of string
+
+Name = 'Saheeb'
+print(len(Name))
+#output= 6
+
+
+#Taking input from user & Calculating its length
+
+username = input("What is your name? ")
+length = len(username)
+print(length)
+#output= What is your name? _Saheeb
+#6
+
+
+#Day 1 Project - BAND NAME GENERATOR
+
+print("Welcome to the Band Name Generator.")
+city = input("Which city did you grow up in?\n")
+pet = input("What is the name of a pet?\n")
+print("Your band name could be: " + city + " " + pet)
+#output= Welcome to the Band Name Generator.
+#Which city did you grow up in?
+#_Baramulla
+#What is the name of a pet?
+#_Tiger
+#Your band name could be: Baramulla Tiger.
+
+
+
+#Day 1 is completed!
